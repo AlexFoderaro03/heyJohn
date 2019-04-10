a4cf6097f4e2f085cfd151953a5c88737d9c745f
alexfoderaro03 <alexfoderaro03@gmail.com>
2019-04-10 17:44:27 +0200

    ...

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import speech_recognition as sr
 import webbrowser
 import subprocess
 import random
+import sys
 from random import choice
 import pyttsx3
 engine = pyttsx3.init()
 
+IS_WINDOWS = sys.platform.lower() == "win32"
 
-r = sr.Recognizer()
+if IS_WINDOWS:
+	r = sr.Recognizer()
+	how_are_you_answers = ["I'm fine thanks!", "I'm good", "I'm feeling very good today!", "Hi, I'm fine!"]
+	ans1 = choice(how_are_you_answers)
 
-how_are_you_answers = ["I'm fine thanks!", "I'm good", "I'm feeling very good today!", "Hi, I'm fine!"]
-ans1 = choice(how_are_you_answers)
-
-with sr.Microphone() as source:
+	with sr.Microphone() as source:
 	print("Say anithing: ")
 	audio = r.listen(source)
 
@@ -58,4 +60,62 @@ with sr.Microphone() as source:
 		print("You said: {}".format(text))
 	except:
 		print("Sorry! I can't understand")
+
+
+
+###UNIX ==> different way to open apps
+
+else:
+    r = sr.Recognizer()
+	how_are_you_answers = ["I'm fine thanks!", "I'm good", "I'm feeling very good today!", "Hi, I'm fine!"]
+	ans1 = choice(how_are_you_answers)
+
+	with sr.Microphone() as source:
+	print("Say anithing: ")
+	audio = r.listen(source)
+
+	try:
+		text = r.recognize_google(audio)
+		file1 = open("./conversation.txt", "w")
+		file1.write(text)
+		file1.close
+
+		if text == "ehi john" or text == "ei John" or text == "hey John" or text == "hey john":
+			engine.say("Yeeesss")
+			engine.runAndWait()
+		elif text == "open YouTube" or text == "open youtube":
+			website = "https://www.youtube.com"
+			webbrowser.open_new_tab(website)
+			engine.say("Sure")
+			engine.runAndWait()
+		elif text == "open Google" or text == "open google":
+			website = "https://www.google.com"
+			webbrowser.open_new_tab(website)
+			engine.say("Sure")
+			engine.runAndWait()
+		elif text == "hi, how are you?" or text == "hi how are you" or text == "hi how are you?":
+			engine.say(ans1)
+			engine.runAndWait()
+		elif text == "how are you?" or text == "how are you" or text == "how are you?":
+			engine.say(ans1)
+			engine.runAndWait()
+		elif text == "open gmail" or text == "open gimail" or text == "open Gmail?":
+			website = "https://mail.google.com/mail/u/0/"
+			webbrowser.open_new_tab(website)
+			engine.say("Sure")
+			engine.runAndWait()
+		elif text == "open instagram" or text == "open Instagram":
+			website = "https://www.instagram.com/"
+			webbrowser.open_new_tab(website)
+			engine.say("Sure")
+			engine.runAndWait()
+
+			
+			
+			
+		print("You said: {}".format(text))
+	except:
+		print("Sorry! I can't understand")
+
+
 
