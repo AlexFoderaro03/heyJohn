f5444701b7d123595c2fc43c0754deaa5950f177
alexfoderaro03 <alexfoderaro03@gmail.com>
2019-04-10 17:57:38 +0200

    ...

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ try:
 		webbrowser.open_new_tab(website)
 		engine.say("Sure")
 		engine.runAndWait()
-	elif text == "run python game" or text == "run Python game" or text == "ran python game" or text == "ran paiton game":
+	elif text == "run python game" or text == "run Python game" or text == "ran python game" or text == "ran paiton game" or text == "open Python game" or text == "open python game":
 		run("/Users/alexfoderaro/Coding/Git Projects/python-game/main.py")
 		engine.say("Sure")
 		engine.runAndWait()
