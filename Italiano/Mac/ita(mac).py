3cd3ece33c487951581b309ca3a1aca1c94a56ba
alexfoderaro03 <alexfoderaro03@gmail.com>
2019-04-20 17:07:06 +0200

    ...

--- Italiano/Mac/ita(mac).py
+++ Italiano/Mac/ita(mac).py
@@ -158,15 +158,15 @@ def john():
 					engine.runAndWait()
 					os.system("open /Applications/iTunes.app")
 
-				elif text == "Apri Spotify" or text == "apri Spotify":
-					engine.say("Certo! Sto aprendo Spotify")
-					engine.runAndWait()
-					os.system("open /Applications/Spotify.app")
+			elif text == "Apri Spotify" or text == "apri Spotify":
+				engine.say("Certo! Sto aprendo Spotify")
+				engine.runAndWait()
+				os.system("open /Applications/Spotify.app")
 
-				elif text == "Apri iTunes" or text == "apri iTunes":
-					engine.say("Certo! Sto aprendo iTunes")
-					engine.runAndWait()
-					os.system("open /Applications/iTunes.app")
+			elif text == "Apri iTunes" or text == "apri iTunes":
+				engine.say("Certo! Sto aprendo iTunes")
+				engine.runAndWait()
+				os.system("open /Applications/iTunes.app")
 
 
 
